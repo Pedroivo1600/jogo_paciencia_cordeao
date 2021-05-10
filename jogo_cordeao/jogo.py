73f7cc9714c4429b5c047a4cf58c823339ddfd6b
Pedro Ivo <pedroir@al.inser.edu.br>
2021-05-10 13:45:55 -0300

    primeira tentativa do jogo

--- jogo_cordeao/jogo.py
+++ jogo_cordeao/jogo.py
@@ -13,4 +13,54 @@ print('2. Empilhar uma carta sobre a terceira carta anterior.')
 print('\nPara que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida:\n') 
 
 print('1. As duas cartas possuem o mesmo valor ou') 
-print('2. As duas cartas possuem o mesmo naipe.') 
+print('2. As duas cartas possuem o mesmo naipe.\n') 
+
+#JOGO
+import random
+i = 52
+baralho = cria_baralho()
+random.shuffle(baralho)
+
+    
+
+import random
+r = True
+i = 52
+baralho = cria_baralho()
+random.shuffle(baralho)
+
+while possui_movimentos_possiveis(baralho):
+    for carta in baralho:
+        print('{}. {}'.format(baralho.index(carta)+1, carta))
+    q = int(input('Digite o número da carta que você escolheu entre 1 e {}:'.format(i)))
+    if q <= i:
+        if lista_movimentos_possiveis(baralho, q-1)!=[]:
+            if lista_movimentos_possiveis(baralho, q-1) == [3]:
+                empilha(baralho, q-1, q-4)
+                i-=1   
+            if lista_movimentos_possiveis(baralho, q-1) == [1]:
+                empilha(baralho, q-1, q-2)
+                i-=1
+            if lista_movimentos_possiveis(baralho, q-1) == [1, 3]:
+                t =True
+                while t:
+                    q2 = int(input('Sobre qual carta você quer empilhar o {}?: \n 1.{} \n 2.{}\n'.format(baralho[q-1], baralho[q-2], baralho[q-4])))
+                    if q2 == 1:
+                        empilha(baralho, q-1, q-2)
+                        i-=1
+                        t = False
+                    if q2 == 2:
+                        empilha(baralho, q-1, q-4)
+                        i-=1
+                        t = False
+                    else:
+                        print('Escolha 1 ou 2')
+                        t = True
+        else:
+            print('A carta {} não pode ser empilhada!'.format(baralho[q-1]))
+    else:
+        print('Digite um número entre 1 e {}'.format(i))
+if len(baralho)==1:
+    print('Você venceu!')
+else:
+    print('Você perdeu!')
