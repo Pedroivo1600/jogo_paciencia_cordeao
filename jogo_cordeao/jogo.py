de21debb34c8173c86ef44bfb47af4d4392f8625
Pedro Ivo <pedroir@al.inser.edu.br>
2021-05-10 12:59:44 -0300

    introdução ao jogo

---
+++ jogo_cordeao/jogo.py
@@ -0,0 +1,16 @@
+from funcoes import *
+
+print('Paciência Acordeão')
+print('\n==================\n') 
+
+print('\nSeja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha.\n') 
+
+print('\nExistem apenas dois movimentos possíveis:\n') 
+
+print('1. Empilhar uma carta sobre a carta imediatamente anterior;') 
+print('2. Empilhar uma carta sobre a terceira carta anterior.') 
+
+print('\nPara que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida:\n') 
+
+print('1. As duas cartas possuem o mesmo valor ou') 
+print('2. As duas cartas possuem o mesmo naipe.') 
