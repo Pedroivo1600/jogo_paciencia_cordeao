db685bbbb5e33fdbb51e3fff9385327faa764959
Pedro Ivo <pedroir@al.inser.edu.br>
2021-05-10 22:24:22 -0300

    jogo final

--- jogo_cordeao/funcoes.py
+++ jogo_cordeao/funcoes.py
@@ -74,3 +74,13 @@ def possui_movimentos_possiveis(baralho):
             return True
         i += 1
     return False
+
+RED   = "\033[1;31m"  
+BLUE  = "\033[1;34m"
+CYAN  = "\033[1;36m"
+GREEN = "\033[0;32m"
+RESET = "\033[0;0m"
+BOLD    = "\033[;1m"
+REVERSE = "\033[;7m"
+
+# print(BLUE+'oi' + RESET)
